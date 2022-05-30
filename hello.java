546a61bc8a66411df43ff47e378b2af1a474a942
RKowshik10 <98247573+RKowshik10@users.noreply.github.com>
2022-05-30 11:58:42 +0530

    Update and rename hello.py to hello.java

---
+++ hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+ static String a="kowshik";
+
+
+
+
+	public static void main(String[]args) 
+	
+	{
+		
+System.out.println("hello java");
+System.out.println(a);
+
+	}
+
+}
